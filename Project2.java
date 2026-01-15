4fac2968fb6b6b9ffa084d8d031488a3965e8f07
leandro-amaro <leanstudy9@gmail.com>
2026-01-15 12:54:05 -0300

    Prática e exercicios básicos em Java

---
+++ Project2.java
@@ -0,0 +1,22 @@
+public class Project2 {
+
+    public static void main(String[] args) {
+
+        double area, h, b, B, resultado;
+        int a, c;
+
+        h = 5.0;
+        B = 8.0;
+        b = 6.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println("a área do trapézio é: " + area);
+        a = 5;
+        c = 2;
+
+        resultado = (double) a/ c;
+        
+        System.out.println(resultado);
+    }
+}
